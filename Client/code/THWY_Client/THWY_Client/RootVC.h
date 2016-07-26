2213f72f5b531494bc33e67c8c2c71626b3a4bfe
admin_sxz <15542185472@163.com>
2016-07-26 15:23:47 +0800

    修正业主端RootVC

---
+++ Client/code/THWY_Client/THWY_Client/RootVC.h
@@ -0,0 +1,13 @@
+//
+//  RootVC.h
+//  THWY_Client
+//
+//  Created by 史秀泽 on 2016/7/26.
+//  Copyright © 2016年 SXZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootVC : UIViewController
+
+@end
---
+++ Client/code/THWY_Client/THWY_Client/RootVC.m
@@ -0,0 +1,47 @@
+//
+//  RootVC.m
+//  THWY_Client
+//
+//  Created by 史秀泽 on 2016/7/26.
+//  Copyright © 2016年 SXZ. All rights reserved.
+//
+
+#import "RootVC.h"
+
+@interface RootVC ()
+
+@end
+
+@implementation RootVC
+
+-(instancetype)init
+{
+    if (self = [super init]) {
+        
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    self.view.backgroundColor = [UIColor whiteColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    NSLog(@"didReceiveMemoryWarning");
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
