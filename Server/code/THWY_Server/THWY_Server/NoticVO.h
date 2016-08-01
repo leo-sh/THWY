5e27f6b0ee5aab2834db2f605d3918e99ef37e62
Mr.S <15542185472@163.com>
2016-08-01 10:31:52 +0800

    管理端API添加具体查看注释

---
+++ Server/code/THWY_Server/THWY_Server/NoticVO.h
@@ -0,0 +1,22 @@
+//
+//  NoticVO.h
+//  THWY_Server
+//
+//  Created by Mr.S on 16/8/1.
+//  Copyright © 2016年 SXZ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NoticVO : NSObject
+
+@property NSString              * Id;
+@property NSString              * note_txt_type_id;
+@property NSString              * title;
+@property NSString              * content;
+@property NSString              * ctime;
+@property NSString              * note_type;
+
+-(NoticVO* )initWithJSON:(NSDictionary *)JSON;
+
+@end
