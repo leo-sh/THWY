5274e974a52e41029e91cc3545b74e7c4043f0c0
wei <18242982609@163.com>
2016-08-02 09:36:41 +0800

    add PushSetting

--- Client/code/THWY_Client/THWY_Client/PushSettingVC.m
+++ Client/code/THWY_Client/THWY_Client/PushSettingVC.m
@@ -7,8 +7,15 @@
 //
 
 #import "PushSettingVC.h"
+#import "UMessage.h"
 
 @interface PushSettingVC ()
+
+@property (strong, nonatomic) UIButton *button1;
+@property (strong, nonatomic) UIButton *button2;
+@property (strong, nonatomic) UIButton *button3;
+
+@property (strong, nonatomic) NSArray *bools;
 
 @end
 
@@ -18,6 +25,151 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.title = @"推送设置";
+    [self.view setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"repaire_背景"]]];
+    [self initViews];
+    
+    self.bools = @[@YES, @NO, @NO];
+}
+
+- (void)initViews{
+    
+    UIView *bgView = [[UIView alloc] init];
+    [bgView setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"pushsetting_bg_bai"]]];
+    [self.view addSubview:bgView];
+    
+    [bgView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(self.view.mas_top).offset(10);
+        make.left.mas_equalTo(self.view.mas_left).offset(10);
+        make.right.mas_equalTo(self.view.mas_right).offset(10);
+        make.height.mas_equalTo(self.view.mas_height).multipliedBy(0.5);
+    }];
+    
+    UILabel *accept = [[UILabel alloc] init];
+    accept.text = @"  接受推送";
+    accept.textAlignment = NSTextAlignmentLeft;
+    accept.textColor = [UIColor blackColor];
+    accept.layer.borderWidth = 1;
+    accept.userInteractionEnabled = YES;
+    accept.layer.borderColor = [UIColor lightGrayColor].CGColor;
+    accept.font = [UIFont fontWithName:My_RegularFontName size:17.0];
+    [accept sizeToFit];
+    [bgView addSubview:accept];
+    [accept mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.width.mas_equalTo(bgView.mas_width);
+        make.height.mas_equalTo(bgView.mas_height).multipliedBy(0.2);
+        make.left.mas_equalTo(bgView.mas_left);
+        make.top.mas_equalTo(bgView.mas_top);
+    }];
+    
+    self.button1 = [[UIButton alloc] init];
+    self.button1.tag = 11;
+    [self.button1 setBackgroundImage:[UIImage imageNamed:@"pushsetting_开"] forState:UIControlStateNormal];
+    [self.button1 addTarget:self action:@selector(btnOnclicked:) forControlEvents:UIControlEventTouchUpInside];
+    [accept addSubview:self.button1];
+    [self.button1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.right.mas_equalTo(accept.mas_right).offset(-15);
+        make.centerY.mas_equalTo(accept.mas_centerY);
+        make.height.mas_equalTo(accept.mas_height).multipliedBy(0.5);
+    }];
+    
+    UILabel *lisheng = [[UILabel alloc] init];
+    lisheng.text = @"  铃声提醒";
+    lisheng.textAlignment = NSTextAlignmentLeft;
+    lisheng.textColor = [UIColor blackColor];
+    lisheng.layer.borderWidth = 1;
+    lisheng.userInteractionEnabled = YES;
+    lisheng.layer.borderColor = [UIColor lightGrayColor].CGColor;
+    lisheng.font = [UIFont fontWithName:My_RegularFontName size:17.0];
+    [lisheng sizeToFit];
+    [bgView addSubview:lisheng];
+    [lisheng mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.width.mas_equalTo(bgView.mas_width);
+        make.height.mas_equalTo(bgView.mas_height).multipliedBy(0.2);
+        make.left.mas_equalTo(bgView.mas_left);
+        make.top.mas_equalTo(accept.mas_bottom).offset(-1);
+    }];
+    
+    self.button2 = [[UIButton alloc] init];
+    self.button2.tag = 12;
+    [self.button2 setBackgroundImage:[UIImage imageNamed:@"pushsetting_关"] forState:UIControlStateNormal];
+    [self.button2 addTarget:self action:@selector(btnOnclicked:) forControlEvents:UIControlEventTouchUpInside];
+    [accept addSubview:self.button2];
+    [self.button2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.right.mas_equalTo(accept.mas_right).offset(-15);
+        make.centerY.mas_equalTo(lisheng.mas_centerY);
+        make.height.mas_equalTo(accept.mas_height).multipliedBy(0.5);
+    }];
+
+    UILabel *zhendong = [[UILabel alloc] init];
+    zhendong.text = @"  接受推送";
+    zhendong.textAlignment = NSTextAlignmentLeft;
+    zhendong.textColor = [UIColor blackColor];
+    zhendong.layer.borderWidth = 1;
+    zhendong.userInteractionEnabled = YES;
+    zhendong.layer.borderColor = [UIColor lightGrayColor].CGColor;
+    zhendong.font = [UIFont fontWithName:My_RegularFontName size:17.0];
+    [zhendong sizeToFit];
+    [bgView addSubview:zhendong];
+    [zhendong mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.width.mas_equalTo(bgView.mas_width);
+        make.height.mas_equalTo(bgView.mas_height).multipliedBy(0.2);
+        make.left.mas_equalTo(bgView.mas_left);
+        make.top.mas_equalTo(lisheng.mas_bottom).offset(-1);
+    }];
+    
+    self.button3 = [[UIButton alloc] init];
+    self.button3.tag = 11;
+    [self.button3 setBackgroundImage:[UIImage imageNamed:@"pushsetting_开"] forState:UIControlStateNormal];
+    [self.button3 addTarget:self action:@selector(btnOnclicked:) forControlEvents:UIControlEventTouchUpInside];
+    [accept addSubview:self.button3];
+    [self.button3 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.right.mas_equalTo(accept.mas_right).offset(-15);
+        make.centerY.mas_equalTo(zhendong.mas_centerY);
+        make.height.mas_equalTo(accept.mas_height).multipliedBy(0.5);
+    }];
+    
+}
+
+- (void)btnOnclicked:(UIButton *)sender{
+
+    NSInteger index = sender.tag-11;
+    if ([self.bools[index] boolValue]) {
+        [sender setBackgroundImage:[UIImage imageNamed:@"pushsetting_关"] forState:UIControlStateNormal];
+    }else{
+        [sender setBackgroundImage:[UIImage imageNamed:@"pushsetting_开"] forState:UIControlStateNormal];
+    }
+    
+    __block UIRemoteNotificationType types7 = UIRemoteNotificationTypeAlert| UIRemoteNotificationTypeSound;
+    __block UIUserNotificationType types8 = UIUserNotificationTypeAlert|UIUserNotificationTypeSound|UIUserNotificationTypeBadge;
+    
+    switch (index) {
+        case 0:{//接受推送
+            if (([self.bools[index] boolValue])) {
+                [UMessage unregisterForRemoteNotifications];
+            }else{
+                [UMessage registerForRemoteNotifications];
+            }
+            break;
+        }
+        case 1:{//铃声提醒
+            if ([self.bools[index] boolValue]) {
+                types7 = UIRemoteNotificationTypeNone;
+                types8 = UIUserNotificationTypeNone;
+            }else{
+                types7 = UIRemoteNotificationTypeAlert| UIRemoteNotificationTypeSound;
+                types8 = UIUserNotificationTypeAlert|UIUserNotificationTypeSound|UIUserNotificationTypeBadge;
+            }
+            [UMessage registerForRemoteNotifications:nil withTypesForIos7:types7 withTypesForIos8:types8];
+            break;
+        }
+        case 2:{//震动提醒
+            
+            break;
+        }
+        default:
+            break;
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -25,14 +177,5 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
--- Client/code/THWY_Client/THWY_Client/RepairRecordsVC.m
+++ Client/code/THWY_Client/THWY_Client/RepairRecordsVC.m
@@ -10,6 +10,16 @@
 
 @interface RepairRecordsVC ()
 
+@property (strong, nonatomic) UIButton *switchButton;
+@property (strong, nonatomic) UILabel *leftLabel;
+@property (strong, nonatomic) UILabel *rightLabel;
+@property (assign, nonatomic) NSInteger switchFlag;
+
+@property (strong, nonatomic) UIScrollView *scrollView;
+
+@property (strong, nonatomic) UITableView *tableView;
+@property (strong, nonatomic) UITableView *tableView2;
+
 @end
 
 @implementation RepairRecordsVC
@@ -19,6 +29,108 @@
     // Do any additional setup after loading the view.
     
     self.title = @"报修记录";
+    [self.view setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"repaire_背景2"]]];
+
+    [self initViews];
+    
+}
+
+- (void)initViews{
+    
+    self.switchFlag = 1;
+    self.switchButton = [[UIButton alloc] init];
+    [self.switchButton setBackgroundImage:[UIImage imageNamed:@"repaire_切换标签左"] forState:UIControlStateNormal];
+    [self.switchButton addTarget:self action:@selector(switchLeftRight) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:self.switchButton];
+    
+    [self.switchButton mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.width.mas_equalTo(self.view.mas_width).multipliedBy(0.75);
+        make.height.mas_equalTo(self.switchButton.mas_width).multipliedBy(70/491.0);
+        make.centerX.mas_equalTo(self.view.mas_centerX);
+        make.top.mas_equalTo(self.view).offset(10);
+    }];
+    
+    self.leftLabel = [[UILabel alloc] init];
+    self.leftLabel.text = @"业主报修";
+    self.leftLabel.font = [UIFont fontWithName:My_RegularFontName size:18.0];
+    self.leftLabel.textColor = [UIColor whiteColor];
+    [self.leftLabel sizeToFit];
+    [self.switchButton addSubview:self.leftLabel];
+    [self.leftLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.mas_equalTo(self.switchButton.mas_centerX).multipliedBy(0.5);
+        make.centerY.mas_equalTo(self.switchButton.mas_centerY);
+    }];
+    
+    self.rightLabel = [[UILabel alloc] init];
+    self.rightLabel.text = @"公共报修";
+    self.rightLabel.textColor = My_NAV_BG_Color;
+    self.rightLabel.font = [UIFont fontWithName:My_RegularFontName size:18.0];
+    [self.rightLabel sizeToFit];
+    [self.switchButton addSubview:self.rightLabel];
+    [self.rightLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.mas_equalTo(self.switchButton.mas_centerX).multipliedBy(1.5);
+        make.centerY.mas_equalTo(self.switchButton.mas_centerY);
+    }];
+    
+    self.tableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, My_ScreenW-20, My_ScreenH-94-40)];
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    self.tableView.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
+    self.tableView.separatorColor = [UIColor grayColor];
+    self.tableView.bounces = NO;
+    self.tableView.showsVerticalScrollIndicator = NO;
+    
+    self.tableView2 = [[UITableView alloc] initWithFrame:CGRectMake(My_ScreenW-20, 0, My_ScreenW-20, My_ScreenH-94-40)];
+    self.tableView2.delegate = self;
+    self.tableView2.dataSource = self;
+    self.tableView2.separatorStyle = UITableViewCellSeparatorStyleSingleLine;
+    self.tableView2.separatorColor = [UIColor grayColor];
+    self.tableView2.bounces = NO;
+    self.tableView2.showsVerticalScrollIndicator = NO;
+    
+    
+    self.scrollView = [[UIScrollView alloc] init];
+    self.scrollView.contentSize = CGSizeMake((My_ScreenW-20)*2.0, My_ScreenH-94);
+    self.scrollView.showsVerticalScrollIndicator = NO;
+    self.scrollView.showsHorizontalScrollIndicator = NO;
+    self.scrollView.bounces = NO;
+    self.scrollView.scrollEnabled = NO;
+    [self.view addSubview:self.scrollView];
+    [self.scrollView addSubview:self.tableView];
+    [self.scrollView addSubview:self.tableView2];
+    
+    [self.scrollView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.mas_equalTo(self.switchButton.mas_bottom).offset(10);
+        make.bottom.mas_equalTo(self.view.mas_bottom).offset(-10);
+        make.left.mas_equalTo(self.view.mas_left).offset(10);
+        make.right.mas_equalTo(self.view.mas_right).offset(-10);
+    }];
+    
+    
+    [self.tableView registerNib:[UINib nibWithNibName:@"TextFieldCell" bundle:nil] forCellReuseIdentifier:@"textFieldCell"];
+
+    
+    [self.tableView2 registerNib:[UINib nibWithNibName:@"PaigongCatogerysCell" bundle:nil] forCellReuseIdentifier:@"PaigongCatogerysCell"];
+        
+}
+
+- (void)switchLeftRight{
+    
+    if (self.switchFlag == 1) {
+        self.leftLabel.textColor = My_NAV_BG_Color;
+        self.rightLabel.textColor = [UIColor whiteColor];
+        [self.switchButton setBackgroundImage:[UIImage imageNamed:@"repaire_切换标签右"] forState:UIControlStateNormal];
+        self.scrollView.contentOffset = CGPointMake(self.tableView.frame.size.width, 0);
+        self.tableView2.contentOffset = CGPointMake(0, 0);
+        self.switchFlag = 2;
+    }else if (self.switchFlag == 2){
+        self.leftLabel.textColor = [UIColor whiteColor];
+        self.rightLabel.textColor = My_NAV_BG_Color;
+        [self.switchButton setBackgroundImage:[UIImage imageNamed:@"repaire_切换标签左"] forState:UIControlStateNormal];
+        self.scrollView.contentOffset = CGPointMake(0, 0);
+        self.tableView.contentOffset = CGPointMake(0, 0);
+        self.switchFlag = 1;
+    }
     
 }
 
