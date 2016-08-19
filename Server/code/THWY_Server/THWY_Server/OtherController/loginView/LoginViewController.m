a7e4ccffa1f473112fe7165a9d219593c5e6b790
HYZ666 <839926066@qq.com>
2016-08-20 00:09:35 +0800

    合并更新

--- Server/code/THWY_Server/THWY_Server/OtherController/loginView/LoginViewController.m
+++ Server/code/THWY_Server/THWY_Server/OtherController/loginView/LoginViewController.m
@@ -12,7 +12,7 @@
 #import "userAndPassWordTextField.h"
 #import "BlueCheckButton.h"
 #import "ServicesManager.h"
-//#import "MainVC.h"
+#import "MainVC.h"
 @interface LoginViewController ()<UITextFieldDelegate>
 
 @property (strong, nonatomic) UIScrollView* introScrollView;
@@ -146,13 +146,13 @@
     
 
     
-//    [self.userTF mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.top.equalTo(self.LogoView.mas_bottom).with.offset(userTFTop);
-//        make.centerX.equalTo(self.view.mas_centerX);
-//        make.width.mas_equalTo(userAndPassWordTFWidth);
-//        make.height.mas_equalTo(userAndPassWordHeight);
-//
-//    }];
+    [self.userTF mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.LogoView.mas_bottom).with.offset(userTFTop);
+        make.centerX.equalTo(self.view.mas_centerX);
+        make.width.mas_equalTo(userAndPassWordTFWidth);
+        make.height.mas_equalTo(userAndPassWordHeight);
+
+    }];
     
     [self.userTF setLeftIcon:@"账号" placeholder:@"请输入账号" backgroundColor:[UIColor whiteColor]];
     
@@ -167,24 +167,24 @@
     [TFbackgroundView addSubview:self.passWordTF];
     
     
-//    [self.passWordTF mas_makeConstraints:^(MASConstraintMaker *make) {
-//        make.top.equalTo(self.userTF.mas_bottom).with.offset(passWordTFTop);
-//        make.centerX.equalTo(self.view.mas_centerX);
-//        make.width.mas_equalTo(userAndPassWordTFWidth);
-//        make.height.mas_equalTo(userAndPassWordHeight);
-//    }];
+    [self.passWordTF mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.userTF.mas_bottom).with.offset(1);
+        make.centerX.equalTo(self.view.mas_centerX);
+        make.width.mas_equalTo(userAndPassWordTFWidth);
+        make.height.mas_equalTo(userAndPassWordHeight);
+    }];
     
     [self.passWordTF setLeftIcon:@"登录密码" placeholder:@"请输入密码" backgroundColor:[UIColor whiteColor]];
     
-//    if ([[UDManager getUD]showPassWord]) {
-//        self.passWordTF.text = [[UDManager getUD]getPassWord];
-//    }
+    if ([[UDManager getUD]showPassWord]) {
+        self.passWordTF.text = [[UDManager getUD]getPassWord];
+    }
 }
 
 - (void)createButton
 {
     self.rememberPassWordBtn = [[BlueCheckButton alloc]initDefaultImageName:@"框不带勾" choosedImageName:@"框带勾" title:@"记住密码"];
-//    self.rememberPassWordBtn.chooseStatu = [[UDManager getUD]showPassWord];
+    self.rememberPassWordBtn.chooseStatu = [[UDManager getUD]showPassWord];
     [self.view addSubview:self.rememberPassWordBtn];
 
     CGFloat rememberPassWordBtnLeft = self.view.width *0.056;
@@ -286,25 +286,25 @@
     }
     
     [SVProgressHUD showWithStatus:@"登录中..."];
-//    [[ServicesManager getAPI] login:self.userTF.text password:self.passWordTF.text savePassWord:self.rememberPassWordBtn.chooseStatu onComplete:^(NSString *errorMsg, UserVO *user) {
-//        NSLog(@"%@",user);
-//        if (errorMsg) {
-//            NSLog(@"%@",errorMsg);
-//            
-//            [SVProgressHUD showErrorWithStatus:errorMsg];
-//            
-//            [[NSNotificationCenter defaultCenter] postNotificationName:Login_Fail object:nil];
-//
-//        }
-//        else {
-//            
-//            [SVProgressHUD dismiss];
-//            [[NSNotificationCenter defaultCenter] postNotificationName:Login_Success object:nil];
-//            
-//            [self dismissViewControllerAnimated:YES completion:nil];
-//        }
-//        
-//    }];
+    [[ServicesManager getAPI] login:self.userTF.text password:self.passWordTF.text savePassWord:self.rememberPassWordBtn.chooseStatu onComplete:^(NSString *errorMsg, UserVO *user) {
+        NSLog(@"%@",user);
+        if (errorMsg) {
+            NSLog(@"%@",errorMsg);
+            
+            [SVProgressHUD showErrorWithStatus:errorMsg];
+            
+            [[NSNotificationCenter defaultCenter] postNotificationName:Login_Fail object:nil];
+
+        }
+        else {
+            
+            [SVProgressHUD dismiss];
+            [[NSNotificationCenter defaultCenter] postNotificationName:Login_Success object:nil];
+            
+            [self dismissViewControllerAnimated:YES completion:nil];
+        }
+        
+    }];
 
 }
 
