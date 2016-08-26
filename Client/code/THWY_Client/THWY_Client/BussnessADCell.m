f96aff18be5d113d5bdea5932573258997b0d522
wei <18242982609@163.com>
2016-08-26 13:44:25 +0800

    提交 业主端 商圈公告 加载更多 webView  重复的bug

--- Client/code/THWY_Client/THWY_Client/BussnessADCell.m
+++ Client/code/THWY_Client/THWY_Client/BussnessADCell.m
@@ -56,7 +56,9 @@
             }
         }
         
-        self.webView = [[UIWebView alloc]initWithFrame:CGRectMake(self.desc.x, self.desc.y, My_ScreenW-36, 100)];
+        if (!self.webView){
+            self.webView = [[UIWebView alloc]initWithFrame:CGRectMake(self.desc.x, self.desc.y, My_ScreenW-36, 100)];
+        }
         
         self.webView.scrollView.bounces = NO;
         self.webView.backgroundColor = My_clearColor;
