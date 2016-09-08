bbcce64df3c1a1c61e6958bd939497f6824e3dad
wei <18242982609@163.com>
2016-09-08 22:39:11 +0800

    修复 业主 报修类别 适配问题

--- Client/code/THWY_Client/THWY_Client/RepairDetailController.m
+++ Client/code/THWY_Client/THWY_Client/RepairDetailController.m
@@ -192,10 +192,11 @@
 
     }else if (indexPath.section == 1){
         CGFloat height = 0;
+        NSLog(@"%@    %@",indexPath, self.model.classes_str);
         if (self.model.classes_str && ![self.model.classes_str isEqualToString:@""]) {
             NSDictionary *dic = [NSDictionary dictionaryWithObjectsAndKeys:[UIFont fontWithName:My_RegularFontName size:15.0],NSFontAttributeName, nil];
             CGRect rect = [self.model.classes_str boundingRectWithSize:CGSizeMake(self.tableView.width*3.0/4, 2000) options:NSStringDrawingUsesLineFragmentOrigin attributes:dic context:nil];
-            height = rect.size.height+20;
+            height = rect.size.height+30;
         }else{
             height = 44;
         }
@@ -210,14 +211,15 @@
                 if (indexPath.row == 2) {
                     return height;
                 }else{
-                    if (indexPath.row == 1) {
-                        return height;
-                    }else{
-                        return 44;
-                    }                }
+                    return 44;
+                }
             }
         }else{
-            return 44;
+            if (indexPath.row == 1) {
+                return height;
+            }else{
+                return 44;
+            }
         }
 
     }else{
